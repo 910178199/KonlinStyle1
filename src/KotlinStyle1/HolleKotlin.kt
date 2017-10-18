d28fe681baf3a81c94157f2791ff7dc4b9cd7e87
lijian <910178199@qq.com>
2017-10-18 17:32:58 +0800

    第二天：学习了基本数据类型

--- src/KotlinStyle1/HolleKotlin.kt
+++ src/KotlinStyle1/HolleKotlin.kt
@@ -8,6 +8,9 @@ package KotlinStyle1
  *  2.lambda(匿名函数)
  *
  *  二,定义常量与变量
+ *     可变变量定义：var 关键字
+ *     不可变变量定义：val 关键字，只能赋值一次的变量(类似Java中final修饰的变量)
+ *     常量与变量都可以没有初始化值,但是在引用前必须初始化
  *
  *  三,字符串模板
  *
@@ -19,6 +22,30 @@ package KotlinStyle1
  *
  */
 
+//常量赋值
+val a: Int = 1
+val b = 1//自动推断为Int类型
+val c: Int = 2
+
+//变量赋值
+var d: Int = 3
+var e = 4
+
+/**
+ * NULL检查机制
+ *
+ */
+
+//类型后面加？表示可为空
+var age: String? = "123"
+//抛出空指针异常
+var ages = age!!.toInt()
+//不做处理返回null
+val ages1 = age?.toInt()
+//age为空返回-1
+val ages2 = age?.toInt() ?: -1
+
+
 fun main(args: Array<String>) {
     println("Hello kotlin")
 
@@ -28,7 +55,27 @@ fun main(args: Array<String>) {
 
     vars(1, 2, 3, 4, 5, 6, 7, 8)
 
+    /**
+     * lambda(匿名函数)
+     */
+    val sumLambda: (Int, Int) -> Int = { x, y -> x + y }
+    println(sumLambda(1, 4))
 
+    //字符串模板
+    StrMode()
+
+    println(age)
+
+    //返回值为null
+//    returnNull()
+
+    //类型监测及自动监测
+    println(getStringLength("1"))
+    println(getStringLength2("22"))
+    println(getStringLength3("333"))
+
+    //区间
+    section()
 }
 
 fun sum(a: Int, b: Int): Int {//int参数，返回int
@@ -65,7 +112,114 @@ fun vars(vararg v: Int) {
 }
 
 
+/**
+ *  字符串模板
+ *  $表示一个变量名或变量值
+ *  $varName 表示变量值
+ *  ${varName.fun()} 表示变量的方法返回值:
+ */
+fun StrMode() {
+    var a = 1;
+    //模板中的简单名称
+    val s1 = "a is $a"
 
+    a = 2
+    //模板中的任意表达式
+    val s2 = "${s1.replace("is", "是")}，but is $a"
+    print(s2)
+}
+
+
+/**
+ * 当str中的字符串内容不是一个整数时，返回null
+ */
+fun parseInt(str: String): Int? {
+    return str.toInt()
+}
+
+//使用一个返回值可为 null 的函数:
+fun returnNull(args: Array<String>) {
+    if (args.size < 2) {
+        print("Two integers expected")
+        return
+    }
+    val x = parseInt(args[0])
+    val y = parseInt(args[1])
+    if (x != null && y != null) {
+        println(x * y)
+    }
+}
+
+
+/**
+ * 类型监测及自动类型转换
+ *  is 运算符检测一个表达式是否某类型的一个实例(类似于Java中的instanceof关键字)。
+ *
+ */
+fun getStringLength(obj: Any): Int? {
+    if (obj is String) {
+        // 做过类型判断以后，obj会被系统自动转换为String类型
+        return obj.length
+    }
+
+    //在这里还有一种方法，与Java中instanceof不同，使用!is
+    /*if (obj !is String){
+
+    }*/
+
+    return null
+}
+
+//或这样写
+fun getStringLength2(obj: Any): Int? {
+
+    if (obj !is String) {
+        return null
+    }
+// 在这个分支中, `obj` 的类型会被自动转换为 `String`
+    return obj.length
+}
+
+//甚至可以这样
+fun getStringLength3(obj: Any): Int? {
+    // 在&&运算符的右侧，obj将自动转换为String
+    if (obj is String && obj.length > 0) {
+        return obj.length
+    }
+    return null
+}
+
+
+/**
+ * 区间
+ *  区间表达式由具有操作符形式 .. 的 rangeTo 函数辅以 in 和 !in 形成。
+ */
+fun section() {
+
+    for (i in 1..4) print(i)//输出“1-100”
+
+    for (i in 4..1) println(i)//什么都不输出
+
+    var i: Int = 9
+    if (i in 1..10) { // 等同于 1 <= i && i <= 10
+        println(i)
+    }
+
+    //使用step指定步长
+    for (i in 1..10 step 2) print(i)//13579
+
+    println()
+
+    for (i in 10 downTo 3 step 2) print(i)//downTo到哪一个数停止
+
+    println()
+
+    //使用until函数排除结束元素
+    for (i in 1 until 10) {
+        print(i)
+    }
+    
+}
 
 
 
---
+++ src/KotlinStyle2/BaseDataType.kt
@@ -0,0 +1,202 @@
+package KotlinStyle2
+
+/**
+ * Kotlin基本数据类型
+ *  Kotlin 的基本数值类型包括 Byte、Short、Int、Long、Float、Double 等。
+ *  不同于Java的是，字符不属于数值类型，是一个独立的数据类型。
+ *
+ *
+ *
+ */
+
+fun main(args: Array<String>) {
+
+
+    //比较两个数字
+    compareValue()
+
+
+}
+
+/**
+ * Kotlin 中没有基础数据类型，只有封装的数字类型，你每定义的一个变量，
+ * 其实 Kotlin 帮你封装了一个对象，这样可以保证不会出现空指针
+ *
+ * 三个等号 === 表示比较对象地址，两个 == 表示比较两个值大小。
+ */
+fun compareValue() {
+    val a: Int = 10000
+    println(a === a) //true,值相等，对象地址相等
+
+    //经历装箱，创建了两个不同的对象
+    val boxedA: Int? = a
+    val auotherBoxedA: Int? = a
+
+    println(boxedA === auotherBoxedA)//false，对象地址不一样
+    println(boxedA == auotherBoxedA)//true,值相等
+
+    /**
+     * 字符
+     * 和 Java 不一样，Kotlin 中的 Char 不能直接和数字操作，Char 必需是单引号''包含起来的。
+     * 支持这几个转义序列：\t、 \b、\n、\r、\'、\"、\\ 和 \$
+     */
+    fun check(c: Char) {
+        /*if (c == 1) { 错误
+
+        }*/
+        if (c == '1') {
+
+        }
+    }
+
+    //把字符串转换为数字
+    println(decimalDiagitValue('9'))
+
+    /**
+     * 逻辑运算符
+     * || – 短路逻辑或
+     *  && – 短路逻辑与
+     *  ! - 逻辑非
+     */
+
+    /**
+     * 数组：用Array实现，size,get,set
+     * 创建的两种方式：
+     * 1.arrayOf()
+     * 2.使用工厂函数
+     *
+     * ByteArray, ShortArray, IntArray...，用来表示各个类型的数组，
+     * 省去了装箱操作，因此效率更高，其用法同Array一样
+     */
+    useArray()
+
+    /**
+     * 字符串
+     *
+     */
+    useString()
+
+}
+
+/**
+ * 字符串操作
+ */
+fun useString() {
+    val str: String = "string"
+
+
+    for (c in str) {
+        println(c)
+    }
+
+    println(str[5])
+
+    //Kotlin 支持三个引号 """ 扩起来的字符串，支持多行字符串
+    val text = """
+       哈哈
+        哈哈
+        哈哈
+    """
+    println(text)
+
+    //String 可以通过 trimMargin() 方法来删除多余的空白。
+    val text2 = """
+        1
+        2
+        3
+    """.trimIndent()
+    println(text2)
+
+    //使用字符串模板
+    useStrMode()
+}
+
+fun useStrMode() {
+
+    val s = "reboot"
+    val str = "$s.length is ${s.length}"
+    println(str)
+
+    // 如果你需要在原生字符串中表示字面值 $ 字符（它不支持反斜杠转义）
+    val price = """
+    ${'$'}9.99
+    """
+    println(price)
+}
+
+/**
+ * 数组
+ */
+fun useArray() {
+    //[1,2,3]
+    val a = arrayOf(1, 2, 3)
+    //[0,2,4]
+    //使用工厂函数
+    val b = Array(3, { i -> (i * 2) })
+
+    println(a[0])
+    println(a[1])
+    println(a[2])
+
+    println(b[0])
+    println(b[1])
+    println(b[2])
+
+    val x: IntArray = intArrayOf(1, 2, 3)
+    x[0] = x[1] + x[2]
+    println(x[0])
+}
+
+
+/**
+ * 类型转换
+ *
+ * 由于不同的表示方式，较小类型并不是较大类型的子类型，较小的类型不能隐式转换为较大的类型。
+ * 这意味着在不进行显式转换的情况下我们不能把 Byte 型值赋给一个 Int 变量。
+ *
+ *
+ */
+fun typeTransformation() {
+    val b: Byte = 1
+//    val i: Int = b 报错
+    val i: Int = b.toInt()
+
+    //有些情况下也是可以使用自动类型转化的，
+    // 前提是可以根据上下文环境推断出正确的数据类型而且数学操作符会做相应的重载
+    val l = 1L + 2 //Long+Int = Long
+}
+
+
+/**
+ * 对于Int和Long类型，还有一系列的位操作符可以使用
+ *  shl(bits) – 左移位 (Java’s <<)
+ *  shr(bits) – 右移位 (Java’s >>)
+ *  ushr(bits) – 无符号右移位 (Java’s >>>)
+ *  and(bits) – 与
+ *  or(bits) – 或
+ *  xor(bits) – 异或
+ *  inv() – 反向
+ */
+fun bitsOperation() {
+    var a: Int = 1
+    var b: Int = 2;
+
+    a shl b
+    a shr b
+    a ushr b
+    a and b
+    a or b
+    a xor b
+}
+
+
+fun decimalDiagitValue(c: Char): Int {
+    //当需要可空引用时，像数字、字符会被装箱。装箱操作不会保留同一性。
+    if (c !in '0'..'9')
+        throw IllegalArgumentException("Out of range")
+    return c.toInt() - '0'.toInt()
+}
+
+
+
+
